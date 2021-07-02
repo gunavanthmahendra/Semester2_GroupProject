d50ab2bb3a0932bdae54084cab08de550c987db8
gunavanthmahendra <mgunavanth@gmail.com>
2021-07-03 01:23:41 +0530

    Code cleaning and new seasonal plot

--- Covid19.R
+++ Covid19.R
@@ -23,23 +23,7 @@ summer = c(as.Date("2020-03-01"): as.Date("2020-05-31"))
 monsoon = c(as.Date("2020-06-01"): as.Date("2020-07-27"))
 
 ## Grouping by Season and WHO Regions
-seasonal_data = cleaned_data
-seasonal_data["Season"] = ""
-i = 1
-while (i <= nrow(data.frame(seasonal_data))) {
-  if (seasonal_data$Date[i] %in% winter) {
-    seasonal_data$Season[i] = "Winter"
-  } 
-  else if (seasonal_data$Date[i] %in% summer) {
-    seasonal_data$Season[i] = "Summer"
-  } 
-  else if (seasonal_data$Date[i] %in% monsoon) {
-    seasonal_data$Season[i] = "Monsoon"
-  }
-  i = i + 1
-}
-
-regional_data = seasonal_data %>% 
+regional_data = cleaned_data %>% 
   group_by(across(c("WHO.Region", "Date"))) %>%
   summarise(
     Confirmed = sum(Confirmed),
@@ -47,11 +31,34 @@ regional_data = seasonal_data %>%
     Recovered = sum(Recovered),
     Active = sum(Active),
   )  
+regional_data["Season"] = ""
+i = 1
+while (i <= nrow(data.frame(regional_data))) {
+  if (regional_data$Date[i] %in% winter) {
+    regional_data$Season[i] = "Winter"
+  } 
+  else if (regional_data$Date[i] %in% summer) {
+    regional_data$Season[i] = "Summer"
+  } 
+  else if (regional_data$Date[i] %in% monsoon) {
+    regional_data$Season[i] = "Monsoon"
+  }
+  i = i + 1
+}
 
-# Making the plots
+# Master Plot
 base_line_plot = ggplot(regional_data, mapping = aes(x = Date, y = Confirmed))
 
+# Overall Graph
 base_line_plot + geom_smooth(color = "#C70039", se = FALSE) + 
+  facet_wrap(~ WHO.Region, nrow = 2, scales = c("free")) + 
+  theme_minimal(base_size = 12) + 
+  theme(axis.text.x = element_text(angle = 90)) + 
+  scale_x_date(date_breaks = "1 month", date_labels = "%b") + 
+  labs(x = "", y = "Confirmed Cases")
+
+# Seasonal Graph
+base_line_plot + geom_smooth(mapping = aes(color = Season), se = FALSE) + 
   facet_wrap(~ WHO.Region, nrow = 2, scales = c("free")) + 
   theme_minimal(base_size = 12) + 
   theme(axis.text.x = element_text(angle = 90)) + 
